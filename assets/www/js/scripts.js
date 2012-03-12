0e687c6574a80be7cafb461359ca7ae0cee2a9af
Teemu <teemu.jonkkari@gmail.com>
2012-03-12 16:27:46 +0200

    working set with barcode reader

--- assets/www/js/scripts.js
+++ assets/www/js/scripts.js
@@ -442,6 +442,22 @@ function onDeviceReady() {
 		    	$('#username').val(uname);
 			}
 		})();
+		
+		
+		
+		window.plugins.barcodeScanner.scan( function(result) {
+		        alert("We got a barcode\n" +
+		                  "Result: " + result.text + "\n" +
+		                  "Format: " + result.format + "\n" +
+		                  "Cancelled: " + result.cancelled);
+		    }, function(error) {
+		        alert("Scanning failed: " + error);
+		    }
+		);
+		
+		window.plugins.barcodeScanner.scan();
+		
+		
 
     })(this.jQuery);
 
